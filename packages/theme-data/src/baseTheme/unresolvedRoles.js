9176be1b327e3aa797963315bbae63cac5991c64
Sean Durham <sean@sdurham.net>
2018-10-22 13:08:25 -0700

    feat: add base roles for Avatar

--- packages/theme-data/src/baseTheme/unresolvedRoles.js
+++ packages/theme-data/src/baseTheme/unresolvedRoles.js
@@ -2,6 +2,8 @@ import mapKeys from "../utils/mapKeys";
 import extendTheme from "../utils/extendTheme";
 import basics from "../basics";
 import system from "./system";
+
+import avatar from "./components/avatar";
 import checkbox from "./components/checkbox";
 import formField from "./components/formField";
 import flyout from "./components/flyout";
@@ -27,6 +29,7 @@ const baseThemeConfig = extendTheme(
     mapKeys(basics.spacings, key => `basics.spacings.${key}`),
     mapKeys(system.colorScheme, key => `colorScheme.${key}`),
     mapKeys(system.density, key => `density.${key}`),
+    avatar,
     checkbox,
     formField,
     flyout,
