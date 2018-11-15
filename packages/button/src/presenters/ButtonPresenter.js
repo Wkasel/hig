7c0083b1e78bb8a61f511cad394ca6349108ba0d
Wilbert Mui <wmui51@yahoo.com>
2018-11-15 09:16:20 -0800

    refactor: remove unused dependencies

--- packages/button/src/presenters/ButtonPresenter.js
+++ packages/button/src/presenters/ButtonPresenter.js
@@ -37,6 +37,8 @@ const classNameByWidth = {
 export default class ButtonPresenter extends Component {
   static propTypes = {
     disabled: PropTypes.bool,
+    hasFocus: PropTypes.bool,
+    hasHover: PropTypes.bool,
     icon: PropTypes.node,
     link: PropTypes.string,
     onBlur: PropTypes.func,
@@ -55,6 +57,8 @@ export default class ButtonPresenter extends Component {
   render() {
     const {
       disabled,
+      hasFocus,
+      hasHover,
       icon,
       link,
       onBlur,
